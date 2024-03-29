6327ded76241aa470f13806e45fc449b1ebc7105
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-29 22:47:20 +0600

    add menu model

---
+++ src/models/menuModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const menuSchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    recipe: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const menuModel = mongoose.model("menu", menuSchema);
