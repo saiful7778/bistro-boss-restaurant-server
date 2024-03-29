248f5a767eb1456f9dbfb79369e147fc6b544eed
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-29 16:20:56 +0600

    add login route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { userModel } from "../models/userModel.js";
 import serverHelper from "../utils/server-helper.js";
+import jwt from "jsonwebtoken";
 
 const route = express.Router();
 
@@ -28,6 +29,45 @@ route.post("/register", (req, res) => {
     res.status(201).send({
       success: true,
       message: "User is created",
+    });
+  }, res);
+});
+
+route.post("/login", (req, res) => {
+  const { email, name, userID } = req.body;
+
+  if (!name || !email || !userID) {
+    return res.status(401).send({
+      success: false,
+      message: "Invalid input data",
+    });
+  }
+
+  serverHelper(async () => {
+    const user = await userModel.findOne({ email, userID }, { __v: 0 });
+
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "user not found",
+      });
+    }
+    const token = jwt.sign(
+      {
+        email: user.email,
+        name: user.fullName,
+        role: user.role,
+        userID: user.userID,
+      },
+      // eslint-disable-next-line no-undef
+      process.env.SITE_SECRET,
+      { expiresIn: "1h" },
+    );
+
+    res.status(200).send({
+      success: true,
+      token,
+      user,
     });
   }, res);
 });
