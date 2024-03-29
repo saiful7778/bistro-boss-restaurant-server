7583e78e946b274154b735313014aa076f249cde
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-29 16:13:27 +0600

    add database connection

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
-
-config();
-
+import mongoose from "mongoose";
 import authentication from "./src/routes/auth.js";
 
+config();
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || "5001";
+// eslint-disable-next-line no-undef
+const dbUrl = process.env.DB_CONNECT;
+
+(async () => {
+  try {
+    console.log("connecting...");
+    await mongoose.connect(dbUrl);
+    console.log("connected DB");
+  } catch (err) {
+    console.log("connection failed");
+    console.log(err);
+  }
+})();
 
 const app = express();
 
 app.use(
   cors({
     origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "DELETE", "OPTIONS", "PATCH"],
     optionsSuccessStatus: 200,
-  })
+  }),
 );
 app.use(express.json());
 
